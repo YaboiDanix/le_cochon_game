8de1d9a37d67ae55efd28c409fdff83596724fde
YaboiDanix <Danixz1255@gmail.com>
2026-09-10 11:03:09 +0200

    pseudocode

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
-//
-// Created by danix on 09.09.2026.
-//
+// Tip: lancer un dé a 6 face autant de fois que tu veux
+
+// Tip: si tu fais 1 tu perds tu reset a 0 et ton tour est terminé
+
+// Tip: si tu depasse 20 le jeu est terminé et le score est bloqué
+
