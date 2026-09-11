7afc8e8d165a64809919074529dc61c0182e3cae
YaboiDanix <Danixz1255@gmail.com>
2026-09-12 00:44:48 +0200

    implimented =1 lose score and stop loop

--- main.cpp
+++ main.cpp
@@ -8,12 +8,16 @@ int main() {
     int Player_1 = 0 ;
     srand(time(0));
     // Tip: lancer un dé a 6 face autant de fois que tu veux
+    // Tip: si tu depasse 20 le jeu est terminé et le score est bloqué
     while (Player_1 <= 20) {
         int score = rand() % 6 + 1;
         std::cout << score << std::endl;
+        // Tip: si tu fais 1 tu perds tu reset a 0 et ton tour est terminé
         if (score == 1) {
             Player_1 = 0;
+            break;
         }else Player_1 += score;
+        std::cout << Player_1 << std::endl;
 
 
 
@@ -21,7 +25,7 @@ int main() {
     return Player_1;
 }
 
-// Tip: si tu fais 1 tu perds tu reset a 0 et ton tour est terminé
 
-// Tip: si tu depasse 20 le jeu est terminé et le score est bloqué
+
+
 
